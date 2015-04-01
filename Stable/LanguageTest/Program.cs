a0565129b4479507248989460b27f7b8ff845dfc
hyq1860 <pchero@163.com>
2015-04-01 18:32:53 +0800

    test

---
+++ Stable/LanguageTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageTest
+{
+    class Program
+    {
+        public static int aaa()
+        {
+            int x = 1;
+
+            try
+            {
+                Console.WriteLine("try");
+                Console.WriteLine(x);
+                return ++x;
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                Console.WriteLine("finanlly before:" + x.ToString());
+                ++x;
+                Console.WriteLine("finanlly after:" + x.ToString());
+            }
+            return x;
+        }
+        public static void Main()
+        {
+            var a = aaa();
+            Console.Write(a);
+            Console.ReadLine();
+        }
+
+    }
+}
