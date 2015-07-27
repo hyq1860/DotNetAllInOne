a9168de50d7de9fdc797aad0dbea80520934b9a0
hyq1860 <pchero@163.com>
2015-07-27 18:18:39 +0800

    模板引擎demo

--- Stable/RazorEngineAllInOne/RazorEngineDemo/Program.cs
+++ Stable/RazorEngineAllInOne/RazorEngineDemo/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Configuration;
 using RazorEngine;
+using RazorEngine.Configuration;
+using RazorEngine.Configuration.Xml;
 using RazorEngine.Templating;
 using RazorEngineDemo.ViewModel;
 
@@ -24,7 +29,19 @@ namespace RazorEngineDemo
                 Console.WriteLine(result);
 
                 string template02 = File.ReadAllText(Path.Combine(TemplateFolderPath, "template02.cshtml"));
-                var result02= Engine.Razor.RunCompile(template02, typeof(ReportModel), new ReportModel() { ReportId = "报告编号", Title = "11" });
+                //var templateService = new TemplateService();
+                //RazorEngineService razorEngineService=new RazorEngineService();
+
+                var config = new TemplateServiceConfiguration();
+                // You can use the @inherits directive instead (this is the fallback if no @inherits is found).
+                //config.BaseTemplateType = typeof(HtmlSupportTemplateBase<>);
+                using (var service = RazorEngineService.Create(config))
+                {
+                    var result02=service.RunCompile(template02, typeof (ReportModel),
+                        new ReportModel() {ReportId = "报告编号", Title = "11"});
+                }
+
+                //var result02 = Engine.Razor.RunCompile(template02, typeof(ReportModel), new ReportModel() { ReportId = "报告编号", Title = "11" });
             }
             catch(Exception exception)
             {
