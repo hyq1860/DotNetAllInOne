63366cc2714368b2bdd866e83607d1b655fbdc41
hyq1860 <pchero@163.com>
2015-07-27 16:50:02 +0800

    模板引擎

---
+++ Stable/RazorEngineAllInOne/RazorEngineDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RazorEngine;
+using RazorEngine.Templating;
+using RazorEngineDemo.ViewModel;
+
+namespace RazorEngineDemo
+{
+    class Program
+    {
+        static readonly string TemplateFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Views");
+
+        static void Main(string[] args)
+        {
+            string template = "Hello @Model.Name, welcome to RazorEngine!";
+            try
+            {
+                var result = Engine.Razor.RunCompile(template, "templateKey", null, new { Name = "World" });
+
+                Console.WriteLine(result);
+
+                string template02 = File.ReadAllText(Path.Combine(TemplateFolderPath, "template02.cshtml"));
+                var result02= Engine.Razor.RunCompile(template02, typeof(ReportModel), new ReportModel() { ReportId = "报告编号", Title = "11" });
+            }
+            catch(Exception exception)
+            {
+                
+            }
+
+            
+        }
+    }
+}
---
+++ Stable/RazorEngineAllInOne/RazorEngineDemo/ViewModel/ReportModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorEngineDemo.ViewModel
+{
+    public class ReportModel
+    {
+        /// <summary>
+        /// 报告id
+        /// </summary>
+        public string ReportId { get; set; }
+
+        /// <summary>
+        /// 报告标题
+        /// </summary>
+        public string Title { get; set; }
+    }
+}
